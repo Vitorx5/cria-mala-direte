3a792dcd265a3457bf06e34a2f5c0228771f5153
João Vitor <vguerreiro200@gmail.com>
2025-06-06 16:33:45 -0400

    Cria botão onclick

---
+++ src/js/scripit.js
@@ -0,0 +1,9 @@
+const getColuna = document.getElementById(".criar-coluna");
+const getButton = document.getElementById("adiciona-input");
+const recebeColuna = document.getElementById("recebe-coluna");
+
+getButton.addEventListener("Click",() => {
+    
+    
+
+});
